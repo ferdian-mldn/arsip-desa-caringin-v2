7bc136c6ef8f484b1d0260135780acd2a4d32aaf
ferdi 740 <ferdanmaulidin09@gmail.com>
2026-09-09 09:17:14 +0700

    fix: prevent force https on localhost docker and set APP_ENV local

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
+        if (config('app.env') === 'production' && str_starts_with(config('app.url', ''), 'https://')) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
 
